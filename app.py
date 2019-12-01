9b7fe2ade17cec743be38618d2b72f0ba15f562d
m0n0l1t <mon0l1t@bk.ru>
2019-12-02 00:27:37 +0500

    create first blueprint and add templates

--- app.py
+++ app.py
@@ -1,31 +1,11 @@
-from collections import namedtuple
+from flask import Flask
+from config import Configuration
 
-from flask import Flask, render_template, redirect, url_for, request
+from posts.blueprint import posts
+
 
 app = Flask(__name__)
+app.config.from_object(Configuration)
 
-Message = namedtuple('Message', 'text tag')
-messages = []
+app.register_blueprint(posts, url_prefix='/blog')
 
-
-@app.route('/', methods=['GET'])
-def hello_world():
-    return render_template('index.html')
-
-
-@app.route('/main', methods=['GET'])
-def main():
-    return render_template('main.html', messages=messages)
-
-
-@app.route('/add_message', methods=['POST'])
-def add_message():
-    text = request.form['text']
-    tag = request.form['tag']
-    messages.append(Message(text, tag))
-
-    return redirect(url_for('main'))
-
-
-if __name__ == '__main__':
-    app.run()
