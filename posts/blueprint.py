4786627fc6ecd596ff0fef42dd6743a2269ec8cf
m0n0l1t <mon0l1t@bk.ru>
2019-12-04 00:12:30 +0500

    Add search and form of create post

--- posts/blueprint.py
+++ posts/blueprint.py
@@ -2,13 +2,45 @@ from flask import Blueprint
 from flask import render_template
 
 from models import Post, Tag
+from .forms import PostForm
+from flask import request
+from app import db
+
+from flask import redirect
+from flask import url_for
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 
 
+@posts.route('/create', methods=['POST', 'GET'])
+def create_post():
+
+    if request.method == 'POST':
+        title = request.form.get['title']
+        body = request.form['body']
+
+        try:
+            post = Post(title=title, body=body)
+            db.session.add(post)
+            db.session.commit()
+        except:
+            print('Something wrong')
+
+        form = PostForm()
+        return redirect(url_for('posts/create_post.html', form=form))
+
+    form = PostForm()
+    return render_template('posts/create_posts.html', form=form)
+
+
 @posts.route('/')
 def index():
-    posts = Post.query.all()
+    q = request.args.get('q')
+
+    if q:
+        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q)).all()
+    else:
+        posts = Post.query.order_by(Post.created.desc())
     return render_template('posts/index.html', posts=posts)
 
 
