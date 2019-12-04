8d19f919e4d5df60decf56d9b76b36e5909eafa3
m0n0l1t <mon0l1t@bk.ru>
2019-12-04 14:10:14 +0500

    add auth admin,base custom admin

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ class Configuration(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'postgres://postgres:123@localhost/mydb_wl'
     SECRET_KEY = 'something very secret'
+
+    SECURITY_PASSWORD_SALT = 'salt'
+    SECURITY_PASSWORD_HASH = 'sha512_crypt'
--- models.py
+++ models.py
@@ -1,6 +1,7 @@
 from app import db
 from datetime import datetime
 import re
+from flask_security import UserMixin, RoleMixin
 
 
 def slugify(s):
@@ -49,3 +50,23 @@ class Tag(db.Model):
 
     def __repr__(self):
         return '{}'.format(self.name)
+
+
+roles_users = db.Table('roles_users',
+                       db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+                       db.Column('role_id', db.Integer, db.ForeignKey('role.id'))
+                       )
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(255))
+    active = db.Column(db.Boolean())
+    roles = db.relationship('Role', secondary=roles_users, backref=db.backref('users', lazy="dynamic"))
+
+
+class Role(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True)
+    description = db.Column(db.String(255))
